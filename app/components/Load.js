bb0340d6a8ee47137f05989173625fd46223342c
jason evans <jason@jasons-MacBook-Pro.local>
2016-05-04 09:07:53 -0400

    More work on setting up D3 for load monitor.

--- app/components/Load.js
+++ app/components/Load.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import LoadStore from '../stores/LoadStore';
 import LoadActions from '../actions/LoadActions';
+import ReactDOM from 'react-dom';
+var Chart = require('react-d3-core').Chart;
+var LineChart = require('react-d3-basic').LineChart;
+
 
 class Load extends React.Component {
   constructor(props) {
     super(props);
+    
     this.state = LoadStore.getState();
     this.onChange = this.onChange.bind(this);
   }
@@ -24,8 +29,58 @@ class Load extends React.Component {
   componentWillUnmount() {
     LoadStore.unlisten(this.onChange);
   }
+  
+  renderLineChart() {
+    var data = this.state.loadHistory;
+    if(!data)
+      return null;
+    
+    var width = 700,
+      height = 300,
+      margins = {left: 100, right: 100, top: 50, bottom: 50},
+      title = "User sample",
+      // chart series,
+      // field: is what field your data want to be selected
+      // name: the name of the field that display in legend
+      // color: what color is the line
+      chartSeries = [
+        {
+          field: 'uptime',
+          name: 'uptime',
+          color: '#ff7f0e'
+        }
+      ],
+      // your x accessor
+      x = function(d) {
+        return d.index;
+      }
+      
+    ReactDOM.render(
+      <Chart
+        title={"Taiwan refuse disposal"}
+        width={width}
+        height={height}
+        margins= {margins}
+        >
+        <LineChart
+          margins= {margins}
+          title={"Taiwan refuse disposal"}
+          data={data}
+          width={width}
+          height={height}
+          chartSeries={chartSeries}
+          x={x}
+          xScale={"time"}
+        />
+      </Chart>
+    , document.getElementById('line-chart')
+    )
+    
+  }
 
   render() {
+    this.renderLineChart();
+    
     let loadHistory = this.state.loadHistory.map((load, index) => {
       return (
         <div key={index} className="row">
@@ -42,6 +97,8 @@ class Load extends React.Component {
           <div>
             {loadHistory}
           </div>
+          
+          <div id="line-chart"> </div>
       </div>
     );
   }
