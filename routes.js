fb34c10e72107e7c628a15a3d6a1851857c97477
Jenna <Jenna>
2016-05-07 12:58:36 -0400

    Experimenting with a new cpu load npm library.

--- routes.js
+++ routes.js
@@ -1,17 +1,32 @@
 var _ = require('underscore-node');
 var moment = require('moment');
+var monitor = require("os-monitor");
 
 // app/routes.js
 module.exports = function(app) {
     app.get('/api/load', function(req, res) {
       //var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
       
-      var load = {
-        uptime: Math.floor(Math.random() * 6) + 1 ,
-        time: moment().format("MM/DD/YYYY h:mm:ss")
-      };
-   
-      res.status(200).send(load);
+      cpu.totalLoad(function(error, results) {
+          if(error) {
+            return console.log(error);
+          }
+          
+          // results (single cpu in percent) =>
+          // [8]
+          
+          // results (multi-cpu in percent) =>
+          // [3, 10]
+          
+          var load = {
+            uptime: results,
+            time: moment().format("MM/DD/YYYY h:mm:ss")
+          };
+      
+          res.status(200).send(load);
+      });
+        
+      
     });
 
 };
